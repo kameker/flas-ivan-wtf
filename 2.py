4510f80a8e8b898498177bb542a71fd3b4c0bd80
kameker <vsrnpra2@gmail.com>
2022-03-12 09:41:20 +0300

    Тренеровка в полёте

---
+++ 2.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+@app.route('/')
+@app.route('/training/<prof>')
+def training(prof):
+    param = {}
+    param['title'] = prof
+    param['typet'] = prof
+    return render_template('typeT.html', **param)
+
+
+if __name__ == '__main__':
+    main()
