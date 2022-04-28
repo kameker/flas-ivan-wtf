627b6bb0b549298517f726f4a3ff0a4c7866a3c8
kameker <vsrnpra2@gmail.com>
2022-04-28 17:19:16 +0300

    Личная карточка

--- 2.py
+++ 2.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from json import load
+from random import sample
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -13,6 +15,18 @@ def list_prof(args):
     param = {}
     param['ol_ul'] = args
     return render_template('ol_ul.html', **param)
+
+
+@app.route('/member')
+def member():
+    with open('templates/data_members.json',encoding='UTF8') as inf:
+        d = load(inf)
+        random_member = sample([d[i] for i in d], 1)[0]
+        proflist = ' '.join(sorted([i for i in random_member[3].split(',')], key=lambda x: x))
+    param = {}
+    param['member'] = random_member[2]
+    param['proflist'] = proflist
+    return render_template('lichnaya.html', **param)
 
 
 if __name__ == '__main__':
