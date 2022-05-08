718e11ef592364fe23283983120e2e366817e020
kameker <vsrnpra2@gmail.com>
2022-05-08 15:53:49 +0300

    Двойная защита

--- 2.py
+++ 2.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from json import loads
-from random import sample
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class Login(FlaskForm):
+    username_1 = StringField('Id космонавта', validators=[DataRequired()])
+    password_1 = PasswordField('Пароль', validators=[DataRequired()])
+    username_2 = StringField('Id капитана', validators=[DataRequired()])
+    password_2 = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 def main():
@@ -24,6 +34,20 @@ def member():
         data = loads(f.read())
         param['data'] = data
     return render_template('lichnaya.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        return redirect('/norm')
+    return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+
+@app.route('/norm')
+def success():
+    return render_template('success.html', title='Успешная авторизация')
 
 
 if __name__ == '__main__':
